ef52d65aa10f8786717c97bcd380a45867db9628
johncbraga <johngitrepository@gmail.com>
2025-07-30 15:51:07 -0300

    Update 3.3.4
    - Realocação da função desenvolvida. Testes foram realizados e tudo está rodando normalmente.

--- funcionalidades.py
+++ funcionalidades.py
@@ -39,7 +39,6 @@ def visualizar(database):
 
 
 def registrar(database):
-    from teste_function import verificar_duplicidade
     while True:
         temp = []
         index = len(database)
@@ -73,3 +72,17 @@ def excluir(database):
 def sincronizar_database(database):
     dados_brutos = atualizar_banco(database)
     write('database.txt', dados_brutos)
+
+
+def verificar_duplicidade(database, team):
+    geiger = 0
+    while True:
+        for item in database:
+            if team in database[item]:
+                geiger += 1                
+        if geiger == 1:
+            print('\033[31mEsse time já está registrado! [000 to cancel]\033[m ')
+            geiger = 0
+            team = input('Team:\033[34m ')
+        else:
+            return team
