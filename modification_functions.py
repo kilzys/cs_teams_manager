e955ca2a73cf50993190ad22e4364905b9933718
johncbraga <johngitrepository@gmail.com>
2025-07-30 14:44:00 -0300

    Update 3.3.0
    - Function Dev finalizada. A mesma serve para alterar o banco de dados de forma livre.

--- modification_functions.py
+++ modification_functions.py
@@ -8,7 +8,7 @@ def menu_modf(database):
         if option > 1:
             return
         elif option == 0:
-            pass
+            dev_function(database)
         elif option == 1:
             update_time(database)
 
@@ -22,7 +22,7 @@ def update_time(database):
 
 
 def selecionar_time():
-        print('Digite o ID do time: [000 para cancelar]')
+        print('Digite o ID do time: [000 para cancelar] ')
         id_team = int(input(': '))
         return id_team
 
@@ -58,3 +58,36 @@ def visualizar(database):
         else:
             print(f'\033[1;30m ID: {team} | Team: {database[team][0]}| Wins: {database[team][1]} | Streak: {database[team][2]} | Loses: {database[team][3]} | Points: {database[team][4]} | Majors: {database[team][5]} \033[m')
         index+=1
+
+
+def dev_function(database):
+    visualizar(database)
+    id = int(input('Team ID: '))
+    while True:
+        print(f'\033[34m[0] - {database[id][0]}  \033[m')
+        print(f'\033[34m[1] - Wins: {database[id][1]}  \033[m')
+        print(f'\033[34m[2] - Streak: {database[id][2]}  \033[m')
+        print(f'\033[34m[3] - Loses: {database[id][3]}  \033[m')
+        print(f'\033[34m[4] - Points: {database[id][4]}  \033[m')
+        print(f'\033[34m[5] - Majors: {database[id][5]}  \033[m')
+        print('\033[31m[6+] - FINALIZAR\033[m')
+        option = int(input(': '))
+        if option >= 6:
+            return
+        elif option == 0:
+            new_info = input(f'Team Name: ')
+        elif option == 1:
+            new_info = input(f'Wins: ')
+        elif option == 2:
+            new_info = input(f'Streak: ')
+        elif option == 3:
+            new_info = input(f'Loses: ')
+        elif option == 4:
+            new_info = input(f'Points: ')
+        elif option == 5:
+            new_info = input(f'Majors: ')
+        confirm = input('\033[31mConfirmar a mudaçna dos dados? [s/n]\033[m ')
+        if confirm in 'Ss':
+            database[id][option] = new_info
+        else:
+            print('\033[31mCancelando a mudança...\033[m')
