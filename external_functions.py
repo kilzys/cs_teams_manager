aa7627009c310622ce0174371f62b5bed02b2d01
John Braga <johngitrepository@gmail.com>
2025-08-01 07:36:01 -0300

    Update 4.0.2
    - Instalação das bibliotecas necessárias;
    
    - Criação da pasta venv para manipulação de bibliotecas externas;
    
    - Separação das informações da database a partir de listas para geração do arquivo excel.

--- external_functions.py
+++ external_functions.py
@@ -1,2 +1,20 @@
+import pandas as pd
+import openpyxl
+
 def criarExcel(database):
-    print('wip')
+    index = []
+    names = []
+    wins = []
+    streak = []
+    loses = []
+    points = []
+    majors = []
+    for item in database:
+        index.append(item)
+        names.append(database[item][0])
+        wins.append(database[item][1])
+        streak.append(database[item][2])
+        loses.append(database[item][3])
+        points.append(database[item][4])
+        majors.append(database[item][5])
+    print(index,names,wins,streak,loses,points,majors)
